2f7229e2625f486a8ac857633068f73116bec3a8
Joe Lloyd Higgs <joe@Joes-MacBook-Air.local>
2024-03-06 15:30:14 +0000

    Symbols and rows added

--- project.js
+++ project.js
@@ -1,6 +1,21 @@
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
 
+const SYMBOLS_COUNT = {
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8,
+};
+
+const SYMBOL_VALUES = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+};
 
 const deposit = () => {
   while (true) {
@@ -10,36 +25,36 @@ const deposit = () => {
     if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
       console.log("Invalid deposit amount, try again.");
     } else {
-        return numberDepositAmount;
+      return numberDepositAmount;
     }
   }
 };
 
 const getNumberOfLines = () => {
-    while (true) {
-        const lines = prompt("Enter a number of lines to bet on (1-3): ");
-        const numberOfLines = parseFloat(lines);
-    
-        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
-          console.log("Invalid number of lines, try again.");
-        } else {
-            return numberOfLines;
-        }
-      }
-}
+  while (true) {
+    const lines = prompt("Enter a number of lines to bet on (1-3): ");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines, try again.");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
 
 const getBet = (balance, lines) => {
-    while (true) {
-        const bet = prompt("Enter the bet per line: ");
-        const numberBet = parseFloat(bet);
-    
-        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
-          console.log("Invalid bet, try again.");
-        } else {
-            return numberBet;
-        }
-      }
-}
+  while (true) {
+    const bet = prompt("Enter the bet per line: ");
+    const numberBet = parseFloat(bet);
+
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+      console.log("Invalid bet, try again.");
+    } else {
+      return numberBet;
+    }
+  }
+};
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
